d839ac7bb6e1f70fe0b32693f8dc32499a6f689f
Georgii Saveliev <georgii@saveliev.su>
2012-11-26 18:01:35 -0600

    Fixed last change

--- BillSync/Database_Functions.cs
+++ BillSync/Database_Functions.cs
@@ -109,7 +109,7 @@ namespace BillSync
             IList<Item> itemList = null;
             using (GroupDataContext context = new GroupDataContext(ConnectionString))
             {
-                IQueryable<Item> query = from c in context.Items orderby c.Created select c;
+                IQueryable<Item> query = from c in context.Items where c.ID == item_id select c;
                 itemList = query.ToList();
             }
             return itemList.FirstOrDefault().Group.Name;
