5ca3c6057c577b531aa6a757ec4c51fca53ceaca
Georgii Saveliev <georgii@saveliev.su>
2012-11-26 21:41:32 -0600

    Get items no groupid

--- BillSync/Database_Functions.cs
+++ BillSync/Database_Functions.cs
@@ -92,12 +92,12 @@ namespace BillSync
             return itemList;
         }
 
-        public static IList<Item> GetItemsSortByDueDate(int group_id)
+        public static IList<Item> GetItemsSortByDueDate()
         {
             IList<Item> itemList = null;
             using (GroupDataContext context = new GroupDataContext(ConnectionString))
             {
-                IQueryable<Item> query = from c in context.Items orderby c.Due where c.GroupID == group_id select c;
+                IQueryable<Item> query = from c in context.Items orderby c.Due select c;
                 itemList = query.ToList();
             }
             return itemList;
