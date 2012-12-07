33707a9ddbf32fad138b00a11b67f9241a097991
John Cabaj <cabaj@wisc.edu>
2012-12-07 15:17:10 -0600

    Commit to get updates
    Getting Georgii's updates

--- BillSync/SubmitPayment.xaml.cs
+++ BillSync/SubmitPayment.xaml.cs
@@ -16,6 +16,7 @@ namespace BillSync
     public partial class SubmitPayment : PhoneApplicationPage
     {
         string member_name = null;
+        Member mem = null;
 
         public SubmitPayment()
         {
@@ -30,8 +31,9 @@ namespace BillSync
             GlobalVars.member_name = null;
             textBlock_name.Text = member_name;
 
-            Member m = findMember(member_name);
-
+            mem = findMember(member_name);
+            IList<Item> items = Database_Functions.GetOwedItems(mem.ID);
+            listPicker_items.ItemsSource = items;
         }
 
         private Member findMember(string member_name)
@@ -50,7 +52,14 @@ namespace BillSync
 
         private void ApplicationBarIconButton_Click(object sender, EventArgs e)
         {
-
+            MessageBoxResult m = MessageBox.Show("Are you sure you'd like to submit payment?", "Submit payment?", MessageBoxButton.OKCancel);
+            if (m == MessageBoxResult.OK)
+            {
+                Item i = (Item)listPicker_items.SelectedItem;
+                decimal amount = 0;
+                decimal.TryParse(textBox_payment.Text, out amount);
+                Database_Functions.AddTransaction(i.ID, mem.ID, amount);
+            }
         }
     }
 }
