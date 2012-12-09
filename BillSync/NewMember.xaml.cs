c51e4883b2a3844544f57809721905a10372adc2
Yue Weng Mak <ymak@wisc.edu>
2012-12-09 13:19:33 -0600

    Update BillSync/NewMember.xaml.cs

--- BillSync/NewMember.xaml.cs
+++ BillSync/NewMember.xaml.cs
@@ -15,20 +15,39 @@ namespace BillSync
 {
     public partial class NewMember : PhoneApplicationPage
     {
-       
+        Group new_group = new Group();
+        bool newMem;
+        IList<Group> grp = Database_Functions.GetGroups();
         public NewMember()
         {
             InitializeComponent();
+            this.listPicker_group.ItemsSource = grp;
         }
         private void button_addMembers_Click(object sender, EventArgs e)
         {
-            Database_Functions.AddMember(0,  textBox_name.Text, textBox_email.Text, textBox_phone.Text);
-            NavigationService.Navigate(new Uri("/People.xaml", UriKind.Relative));
+            // Check whether if the member that is added exist in the database
+            newMem = checkMembers(textBox_name.Text);
+            if (newMem)
+            {
+                MessageBox.Show("Member name already exists. Please enter in new name", "Member name exists", MessageBoxButton.OK);
+                textBox_name.Text = "";
+            }
+            else
+            {
+                new_group = (Group)listPicker_group.SelectedItem;
+                Database_Functions.AddMember(new_group.ID, textBox_name.Text, textBox_email.Text, textBox_phone.Text);
+                NavigationService.Navigate(new Uri("/People.xaml", UriKind.Relative));
+            }    
+            
 
         }
         private void textBox_name_Tap(object sender, EventArgs e)
         {
-            
+            InputScope Keyboard = new InputScope();
+            InputScopeName ScopeName = new InputScopeName();
+            ScopeName.NameValue = InputScopeNameValue.Text;
+            Keyboard.Names.Add(ScopeName);
+            textBox_name.InputScope = Keyboard;
         }
         private void textBox_email_Tap(object sender, EventArgs e)
         {
@@ -36,7 +55,7 @@ namespace BillSync
             InputScopeName ScopeName = new InputScopeName();
             ScopeName.NameValue = InputScopeNameValue.EmailSmtpAddress;
             Keyboard.Names.Add(ScopeName);
-            textBox_phone.InputScope = Keyboard;
+            textBox_email.InputScope = Keyboard;
         }
         private void textBox_phone_Tap(object sender, EventArgs e)
         {
@@ -46,6 +65,17 @@ namespace BillSync
             Keyboard.Names.Add(ScopeName);
             textBox_phone.InputScope = Keyboard;
 
+        }
+        private bool checkMembers(string mem_name)
+        {
+            foreach (Member mem in Database_Functions.GetAllMembers())
+            {
+                if (mem.Name.Equals(mem_name))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
