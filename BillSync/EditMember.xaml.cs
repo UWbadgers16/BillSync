e3cd0446e055f4dc4491314fa4b7603ecfdbd34e
Yue Weng Mak <ymak@wisc.edu>
2012-12-09 13:16:56 -0600

    Update BillSync/EditMember.xaml.cs

--- BillSync/EditMember.xaml.cs
+++ BillSync/EditMember.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -15,9 +15,49 @@ namespace BillSync
 {
     public partial class EditMember : PhoneApplicationPage
     {
+
+        Member edit_memb = new Member();
         public EditMember()
         {
             InitializeComponent();
+            
+            edit_memb = GlobalVars.member;
+            member_name.Text = edit_memb.Name;
+            textBox_name.Text = edit_memb.Name;
+            textBox_email.Text = edit_memb.Email;
+            textBox_phone.Text = edit_memb.Phone;
+        }
+
+        private void button_editMembers_Click(object sender, EventArgs e)
+        {
+            Database_Functions.EditMember(edit_memb.ID, textBox_name.Text, textBox_email.Text, textBox_phone.Text);
+            NavigationService.Navigate(new Uri("/People.xaml", UriKind.Relative));
+
+        }
+        private void textBox_name_Tap(object sender, EventArgs e)
+        {
+            InputScope Keyboard = new InputScope();
+            InputScopeName ScopeName = new InputScopeName();
+            ScopeName.NameValue = InputScopeNameValue.Text;
+            Keyboard.Names.Add(ScopeName);
+            textBox_phone.InputScope = Keyboard;
+        }
+        private void textBox_email_Tap(object sender, EventArgs e)
+        {
+            InputScope Keyboard = new InputScope();
+            InputScopeName ScopeName = new InputScopeName();
+            ScopeName.NameValue = InputScopeNameValue.EmailSmtpAddress;
+            Keyboard.Names.Add(ScopeName);
+            textBox_phone.InputScope = Keyboard;
+        }
+        private void textBox_phone_Tap(object sender, EventArgs e)
+        {
+            InputScope Keyboard = new InputScope();
+            InputScopeName ScopeName = new InputScopeName();
+            ScopeName.NameValue = InputScopeNameValue.Digits;
+            Keyboard.Names.Add(ScopeName);
+            textBox_phone.InputScope = Keyboard;
+
         }
     }
 }
